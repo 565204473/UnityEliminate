340998d38b5bc14f53c5734d8c33b9668deea6b2
565204473 <565204473@qq.com>
2018-06-05 22:29:31 +0800

    添加删除的脚本

---
+++ UnityEliminate/Assets/Script/Game/GameInit.cs
@@ -0,0 +1,12 @@
+﻿using QFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInit : MonoBehaviour {
+
+    void Start() {
+        ScenesMgr.Instance.OpenScene(SceneType.SceneLogion);
+        DontDestroyOnLoad(this.transform.gameObject);
+    }
+}
