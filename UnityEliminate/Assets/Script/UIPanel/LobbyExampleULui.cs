a09ec4989761a907fdbc5611cc64a2b2e20d5223
565204473 <565204473@qq.com>
2018-07-03 19:52:15 +0800

    添加 LobbyExampleULui

---
+++ UnityEliminate/Assets/Script/UIPanel/LobbyExampleULui.cs
@@ -0,0 +1,19 @@
+﻿using QFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class LobbyExampleULui : QUIBehaviour {
+
+    protected override void InitUI(IUIData uiData = null) {
+        base.InitUI(uiData);
+    }
+
+    protected override void OnShow() {
+        base.OnShow();
+    }
+
+    protected override void OnHide() {
+        base.OnHide();
+    }
+}
