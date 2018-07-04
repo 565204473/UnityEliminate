978a56522b4d9ca8a716ff774a10f97a10b90c2d
565204473 <565204473@qq.com>
2018-07-04 23:24:17 +0800

    测试一些界面

--- UnityEliminate/Assets/Script/UIPanel/LobbyExampleULui.cs
+++ UnityEliminate/Assets/Script/UIPanel/LobbyExampleULui.cs
@@ -3,17 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class PanelData : UIPageData {
+    public int index = 0;
+    public PanelData(int index) {
+        this.index = index;
+    }
+}
+
 public partial class LobbyExampleULui : QUIBehaviour {
 
+    private PanelData mData;
     protected override void InitUI(IUIData uiData = null) {
-        base.InitUI(uiData);
+        mData = uiData as PanelData;
+        Debug.LogError(mData.index);
     }
 
     protected override void OnShow() {
         base.OnShow();
+
     }
 
     protected override void OnHide() {
         base.OnHide();
+    }
+    protected override void RegisterUIEvent() {
+        base.RegisterUIEvent();
+        UITransitionExtension.BindTransition<LobbyExampleULui, LobbyGameOpenPanel>(BtnBack);
+        UIToos.ButtonAddListener(BtnOpenLobby, OnBtnOpenLobbyClick);
+    }
+
+    private void OnBtnOpenLobbyClick(GameObject go) {
+        UIMgr.OpenPanel<LobbyGameLobbyPanel>();
     }
 }
--- UnityEliminate/Assets/Script/UIPanel/LobbyGameLobbyPanel.cs
+++ UnityEliminate/Assets/Script/UIPanel/LobbyGameLobbyPanel.cs
@@ -2,38 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using QFramework;
-public partial class LobbyGameLobbyPanel : QUIBehaviour
-{
-    protected override void InitUI(IUIData uiData = null)
-    {
+public partial class LobbyGameLobbyPanel : QUIBehaviour {
+    protected override void InitUI(IUIData uiData = null) {
         base.InitUI(uiData);
     }
 
-    protected override void OnShow()
-    {
+    protected override void OnShow() {
         base.OnShow();
     }
 
-    protected override void OnHide()
-    {
+    protected override void OnHide() {
         base.OnHide();
     }
 
-    protected override void RegisterUIEvent()
-    {
+    protected override void RegisterUIEvent() {
         base.RegisterUIEvent();
         UIToos.ButtonAddListener(BtnOpenGameEnd, OnBtnOpenGameEnd);
         UIToos.ButtonAddListener(BtnBtnULui, OnOpenLuiPanel);
     }
 
 
-    private void OnBtnOpenGameEnd(GameObject go)
-    {
+    private void OnBtnOpenGameEnd(GameObject go) {
         QUIManager.Instance.HideUI(this.name);
         ScenesMgr.Instance.OpenScene(SceneType.SceneGame);
     }
 
     private void OnOpenLuiPanel(GameObject go) {
         Debug.Log("打开ui例子界面");
+        UIMgr.HidePanel<LobbyGameLobbyPanel>();
+        UIMgr.OpenPanel<LobbyExampleULui>(UILevel.AlwayTop, new PanelData(1));
     }
 }
